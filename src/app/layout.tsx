5360814b7bbb6f340943d98810d99944d3151188
JuanNilo <juan.nilo@alumnos.ucn.cl>
2025-03-20 20:11:05 -0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/layout/NavBar";
-import Footer from "@/components/layout/Footer";
+// import Footer from "@/components/layout/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
