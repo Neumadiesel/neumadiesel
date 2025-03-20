a3ea8d0320e6b7a535c6aff91f12e4fc3bad7a5f
JuanNilo <juan.nilo@alumnos.ucn.cl>
2025-03-20 20:08:33 -0300

    UPDATE: responsive

--- src/app/maquinaria/layout.tsx
+++ src/app/maquinaria/layout.tsx
@@ -8,11 +8,11 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <div className="bg-[#f1f1f2] dark:bg-[#212121] flex gap-x-5 min-h-screen">
-            <main className="w-[35%] h-screen flex justify-center items-center">
-                <div className="bg-white dark:bg-black shadow-xl rounded-lg w-[95%] h-[95%] ">
+        <div className="bg-[#f1f1f2] dark:bg-[#212121] block md:flex gap-x-5 min-h-screen">
+            <main className="w-full md:w-[35%] md:h-screen h-[45vh]  flex justify-center items-center">
+                <div className="bg-white dark:bg-black shadow-md rounded-lg w-[95%] h-[95%] ">
                     <h2 className="dark:text-amber-300 text-2xl font-bold p-2">Lista de Camiones</h2>
-                    <div className="grid grid-cols-3 h-[90%] overflow-scroll gap-x-4 gap-y-2 px-6">
+                    <div className="grid grid-cols-3 h-[85%] overflow-scroll gap-x-4 gap-y-2 px-6">
                         {Camiones.map((camion) => (
                             <Link href={`/maquinaria/${camion.Codigo}`} key={camion.Codigo} className="flex flex-col h-28 justify-center items-center p-2 bg-[#f1f1f1] dark:bg-[#212121] rounded-md hover:bg-amber-200 transition-all ease-in-out ">
                                 <GiMineTruck size={35} />
@@ -23,7 +23,7 @@ export default function RootLayout({
                     </div>
                 </div>
             </main>
-            <section className="w-[65%] p-2">
+            <section className="w-full md:w-[65%] md:p-2">
                 {children}
             </section>
         </div>
--- src/components/Listas/InfoCamion.tsx
+++ src/components/Listas/InfoCamion.tsx
@@ -29,35 +29,34 @@ export default function ListaMaquinaria() {
     const neumaticos: NeumaticoInt[] = Neumaticos.filter((neumatico: NeumaticoInt) => neumatico.Codigo_Camion === id);
 
     return (
-        <div className="p-4 h-screen w-full mb-4 rounded-md bg-white text-white dark:bg-black relative shadow-md font-mono">
+        <div className="p-4 h-full w-full mb-4 rounded-md bg-white text-white dark:bg-black relative shadow-md font-mono">
             <div className="text-black dark:text-white flex flex-col">
                 {/* Info del camión */}
-                <div className="flex justify-between mb-2">
-                    <h2 className="text-2xl font-bold ">
+                <div className="md:flex justify-between mb-2">
+                    <h2 className="text-2xl font-bold mb-2">
                         Camión {id} - Faena {camion?.Faena}
                     </h2>
-                    <div className="flex flex-col gap-y-1 justify-between w-[40%] items-end gap-x-4">
+                    <div className="flex md:flex-col gap-y-1 justify-between w-[100%] md:w-[40%] items-end gap-x-4">
                         <Link href={`/mantenimiento/${id}`} className="text-black text-lg bg-amber-300 w-52 text-center p-2 rounded-md border border-black">Historial</Link>
                         <Link href={`/mantenimiento/${id}`} className="text-black text-lg w-52 text-center bg-amber-300 p-2 rounded-md border border-black">Mantenimiento</Link>
                     </div>
                 </div>
                 {/* Seccion de informacion */}
-                <div className="flex ">
+                <div className="block md:flex ">
                     {/* Esquema de neumaticos*/}
                     <section className="min-w-[35%]   p-2 flex justify-center items-center">
                         <Image src="/Axle_gray.png" className="drop-shadow-xl" alt="Esquema" width={300} height={200} />
                     </section>
-                    {/* <CardNeumatico neumatico={neumaticos[5]} /> */}
 
                     {/* Lista de neumaticos */}
-                    <div className="w-[65%] h-full">
+                    <div className="w-full md:w-[65%] h-screen">
                         <h2 className="text-2xl font-bold text-black dark:text-white">Neumáticos</h2>
-                        <div className="relative overflow-x-auto h-[80%] my-2">
+                        <div className="relative overflow-x-auto md:h-[80%] my-2">
                             <div className="flex flex-col gap-y-2">
                                 {/* Table head */}
                                 <div className="w-full border-b-[1px] border-b-gray-300 dark:border-b-gray-600 shadow-sm bg-slate-300 dark:bg-[#212121] rounded-md p-2 h-12 transition-all flex items-center justify-between ">
                                     <div className="flex flex-col">
-                                        <p>Datos del neumatico:</p>
+                                        <p className="font-semibold font-mono">Codigo</p>
                                     </div>
                                     <div>
                                         <p className=" font-mono font-semibold">Mediciones</p>
@@ -65,22 +64,22 @@ export default function ListaMaquinaria() {
                                     <div>
                                         <p className=" font-mono font-semibold">Metas</p>
                                     </div>
-                                    <div className="flex justify-center items-center">
+                                    <div className="flex font-mono font-semibold justify-center items-center">
                                         <p>Acciones</p>
                                     </div>
                                 </div>
                                 {neumaticos.map((neumatico: NeumaticoInt) => (
-                                    <div key={neumatico.Codigo} className="w-full border-b-[1px] hover:bg-amber-100 h-20 transition-all flex items-center justify-between ease-in-out border-b-amber-300 p-2">
-                                        <div className="flex flex-col">
-                                            <p>Codigo: {neumatico.Codigo}</p>
-                                            <p>Posicion: {neumatico.Posicion}</p>
+                                    <div key={neumatico.Codigo} className="w-full border-b-[1px] hover:bg-amber-100 h-32 md:h-20 transition-all flex items-center justify-between ease-in-out border-b-amber-300 p-2">
+                                        <div className="flex flex-col w-[30%]">
+                                            <p>{neumatico.Codigo}</p>
+                                            <p><small className="">Pos:</small> {neumatico.Posicion}</p>
                                         </div>
                                         <div>
                                             <p>Interna: {neumatico.Profundidad}</p>
                                             <p>Externa: {neumatico.Profundidad}</p>
                                         </div>
                                         <div>
-                                            <p>Horas: {neumatico.META_HORAS}</p>
+                                            <p>Hr: {neumatico.META_HORAS}</p>
                                             <p>Km: {neumatico.META_KMS}</p>
                                         </div>
                                         <div className="flex justify-center items-center">
