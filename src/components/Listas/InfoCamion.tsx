e9678c54b03de041e9b9294edf1af89e54b3f2fc
JuanNilo <juan.nilo@alumnos.ucn.cl>
2025-03-25 10:07:44 -0300

    UPDATE: Skeleton maquinaria

--- src/components/Listas/InfoCamion.tsx
+++ src/components/Listas/InfoCamion.tsx
@@ -4,7 +4,9 @@ import { Neumaticos } from "@/mocks/neumaticos.json"
 import Image from "next/image"
 import Link from "next/link"
 import { useParams } from "next/navigation"
+import { useState } from "react"
 import { FaClock, FaInfoCircle, FaPodcast } from "react-icons/fa"
+import { FaCircleDot } from "react-icons/fa6"
 
 interface NeumaticoInt {
     Id: string;
@@ -23,7 +25,20 @@ export default function ListaMaquinaria() {
     const params = useParams<{ id: string }>();
     const id = params.id
 
+    console.log(id)
+
+    const [isSensorActive, setIsSensorActive] = useState(false);
+
     const camion = Camiones.find(camion => camion.Codigo === id);
+
+    const sensores = [
+        { posicion: 1, sensor: 39412, psi: 105, temp: 95 },
+        { posicion: 2, sensor: 39412, psi: 105, temp: 95 },
+        { posicion: 3, sensor: 39412, psi: 105, temp: 95 },
+        { posicion: 4, sensor: 39412, psi: 105, temp: 95 },
+        { posicion: 5, sensor: 39412, psi: 105, temp: 95 },
+        { posicion: 6, sensor: 39412, psi: 105, temp: 95 },
+    ]
 
     // Tipar los neumáticos correctamente
     const neumaticos: NeumaticoInt[] = Neumaticos.filter((neumatico: NeumaticoInt) => neumatico.Codigo_Camion === id);
@@ -37,8 +52,24 @@ export default function ListaMaquinaria() {
                         Camión {id} - Faena {camion?.Faena}
                     </h2>
                     <div className="flex md:flex-col gap-y-1 justify-between w-[100%] md:w-[40%] items-end gap-x-4">
-                        <Link href={`/maquinaria/${id}/sensor`} className="text-black text-lg w-52 text-center flex justify-center gap-x-4 items-center bg-amber-300 p-2 rounded-md border border-black"><FaPodcast className="text-2xl text-black " /> Sensores</Link>
-                        <Link href={`/mantenimiento/${id}`} className="text-black text-lg w-52 text-center bg-amber-300 p-2 rounded-md border border-black">Mantenimiento</Link>
+                        <button disabled={id === undefined} onClick={() => setIsSensorActive(!isSensorActive)} className="text-black text-lg w-52 text-center flex justify-center gap-x-4 items-center bg-amber-300 p-2 rounded-md border border-black">
+                            {
+                                isSensorActive
+                                    ? <FaCircleDot className="text-2xl text-black " />
+                                    : <FaPodcast className="text-2xl text-black " />
+                            }
+                            {
+                                isSensorActive ? "Neumaticos" : "Sensores"
+                            }
+                        </button>
+                        {
+                            id !== undefined ? (
+                                <Link href={`/mantenimiento/${id}`} className="text-black text-lg w-52 text-center bg-amber-300 p-2 rounded-md border border-black">Mantenimiento</Link>
+                            ) : (
+                                <div className="text-black text-lg w-52 text-center bg-amber-300 p-2 rounded-md border border-black">-------</div>
+                            )
+                        }
+
                     </div>
                 </div>
                 {/* Seccion de informacion */}
@@ -55,45 +86,88 @@ export default function ListaMaquinaria() {
                             <div className="flex flex-col gap-y-2">
                                 {/* Table head */}
                                 <div className="w-full border-b-[1px] border-b-gray-300 dark:border-b-gray-600 shadow-sm bg-slate-300 dark:bg-[#212121] rounded-md p-2 h-12 transition-all flex items-center justify-between ">
-                                    <div className="flex flex-col">
+                                    <div className="flex flex-col w-[20%]">
                                         <p className="font-semibold font-mono">Codigo</p>
                                     </div>
                                     <div>
                                         <p className=" font-mono font-semibold">Mediciones</p>
                                     </div>
                                     <div>
-                                        <p className=" font-mono font-semibold">Metas</p>
+                                        <p className=" font-mono font-semibold">
+                                            {
+                                                isSensorActive ? "Posicion" : "Metas"
+                                            }
+                                        </p>
                                     </div>
                                     <div className="flex font-mono font-semibold justify-center items-center">
                                         <p>Acciones</p>
                                     </div>
                                 </div>
-                                {neumaticos.map((neumatico: NeumaticoInt) => (
-                                    <div key={neumatico.Codigo} className="w-full border-b-[1px] hover:bg-amber-100 h-32 md:h-20 transition-all flex items-center justify-between ease-in-out border-b-amber-300 p-2">
-                                        <div className="flex flex-col w-[30%]">
-                                            <p>{neumatico.Codigo}</p>
-                                            <p><small className="">Pos:</small> {neumatico.Posicion}</p>
+                                {
+                                    id === undefined && Array.from({ length: 6 }).map((_, index) => (
+                                        <div key={index} className="w-full border-b-[1px] hover:bg-amber-100 h-32 md:h-20 transition-all flex items-center justify-between ease-in-out border-b-amber-300 p-2">
+                                            <div className="flex flex-col gap-y-1 w-[20%]">
+                                                <div className="bg-slate-200  p-2 rounded-md"></div>
+                                                <div className="bg-slate-200 p-2 rounded-md"></div>
+                                            </div>
+                                            <div className="flex flex-col gap-y-1 w-[20%]">
+                                                <div className="bg-slate-200  p-2 rounded-md"></div>
+                                                <div className="bg-slate-200 p-2 rounded-md"></div>
+                                            </div>
+                                            <div className="flex flex-col gap-y-1 w-[20%]">
+                                                <div className="bg-slate-200  p-2 rounded-md"></div>
+                                                <div className="bg-slate-200 p-2 rounded-md"></div>
+                                            </div>
+                                            <div className="flex justify-center items-center">
+                                                <div className="bg-slate-200 p-2 rounded-full "></div>
+                                            </div>
                                         </div>
-                                        <div>
-                                            <p>Interna: {neumatico.Profundidad}</p>
-                                            <p>Externa: {neumatico.Profundidad}</p>
-                                        </div>
-                                        <div>
-                                            <p>Hr:  {neumatico.META_HORAS}</p>
-                                            <p>Km:  {neumatico.META_KMS}</p>
-                                        </div>
-                                        <div className="flex justify-center items-center">
+                                    ))
+                                }
+                                {
+                                    isSensorActive ? (
+                                        sensores.map((sensor) => (
+                                            <div key={sensor.posicion} className="w-full border-b-[1px] hover:bg-amber-100 h-32 md:h-20 transition-all flex items-center justify-between ease-in-out border-b-amber-300 p-2">
+                                                <div className="flex flex-col w-[20%] ">
 
-                                            <Link href={`/neumaticos/${neumatico.Codigo}`} className="flex justify-between items-center h-12 w-12  text-lg ">
-                                                <FaInfoCircle size={20} />
-
-                                            </Link>
-                                            <Link href={`/mantenimiento/Historial`} className="flex justify-between items-center h-12 w-12  text-lg ">
-                                                <FaClock size={20} />
-                                            </Link>
-                                        </div>
-                                    </div>
-                                ))}
+                                                    <p>{sensor.sensor}</p>
+                                                </div>
+                                                <div>
+                                                    <p>PSI: {sensor.psi}</p>
+                                                    <p>Temp °C: {sensor.temp}</p>
+                                                </div>
+                                                <p>Posicion: {sensor.posicion}</p>
+                                                <div className="flex justify-center items-center">
+                                                    <Link href={`/mantenimiento/Historial`} className="flex justify-between items-center h-12 w-12 text-lg">
+                                                        <FaClock size={20} />
+                                                    </Link>
+                                                </div>
+                                            </div>
+                                        ))
+                                    ) : (
+                                        neumaticos.map((neumatico: NeumaticoInt) => (
+                                            <div key={neumatico.Codigo} className="w-full border-b-[1px] hover:bg-amber-100 h-32 md:h-20 transition-all flex items-center justify-between ease-in-out border-b-amber-300 p-2">
+                                                <div className="flex flex-col w-[20%]">
+                                                    <p>{neumatico.Codigo}</p>
+                                                    <p><small className="">Pos:</small> {neumatico.Posicion}</p>
+                                                </div>
+                                                <div>
+                                                    <p>Interna: {neumatico.Profundidad}</p>
+                                                    <p>Externa: {neumatico.Profundidad}</p>
+                                                </div>
+                                                <div>
+                                                    <p>Hr: {neumatico.META_HORAS}</p>
+                                                    <p>Km: {neumatico.META_KMS}</p>
+                                                </div>
+                                                <div className="flex justify-center items-center">
+                                                    <Link href={`/mantenimiento/Historial`} className="flex justify-between items-center h-12 w-12 text-lg">
+                                                        <FaClock size={20} />
+                                                    </Link>
+                                                </div>
+                                            </div>
+                                        ))
+                                    )
+                                }
                             </div>
                         </div>
                     </div>
