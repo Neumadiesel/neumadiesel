88878c77051cba662c14d5824d884d62378ff50d
JuanNilo <juan.nilo@alumnos.ucn.cl>
2025-04-07 11:12:50 -0400

    Refactor layout components for enhanced responsiveness and improved styling across various screen sizes

--- src/app/Ingresar-datos/medicion-por-equipo/page.tsx
+++ src/app/Ingresar-datos/medicion-por-equipo/page.tsx
@@ -22,8 +22,9 @@ export default function Page() {
     const handleInputChange = (index: number, field: string, value: number) => {
         const valorOriginal = medidasNeumaticosIniciales[index][field as keyof typeof medidasNeumaticosIniciales[0]];
 
-
-        if (value > valorOriginal && field !== "tem" && field !== "pre") {
+        if (value < 1) {
+            setError("El valor no puede ser inferior a 1");
+        } else if (value > valorOriginal && field !== "tem" && field !== "pre") {
             setError(`La goma remanente no puede ser mayor a la original (${valorOriginal})`);
         } else {
             setNuevasMedidas((prev) =>
@@ -32,7 +33,6 @@ export default function Page() {
                 )
             );
         }
-
     };
 
     const handleResetRow = (index: number) => {
@@ -50,19 +50,20 @@ export default function Page() {
 
     return (
         <div className="font-mono h-screen lg:h-[100%] gap-y-2">
+            <h1 className='text-3xl mb-2 font-bold'>Ingresar datos de Equipo</h1>
             <section className='h-screen'>
-                <div className='flex items-center gap-x-2 border-b border-b-amber-300 pb-2'>
+                <div className='flex items-center gap-x-2 border-b border-b-amber-300 pb-3'>
                     <label className="text-lg mb-1 text-black font-semibold dark:text-white ">Ingrese código del equipo:</label>
                     <input type="text" className="w-[40%] bg-gray-50 dark:bg-[#414141] rounded-lg border border-amber-300 p-2" />
                 </div>
 
                 <div className="overflow-x-auto mt-4 w-[100%]">
                     {/* Fecha medicion anterior */}
-                    <div className='flex justify-between'>
+                    <div className='flex justify-between mb-2'>
                         <p className="text-sm text-gray-700 dark:text-white">Ultima medición del equipo <span className='font-semibold'>29/03/2025</span></p>
                     </div>
                     {/* Ingresar datos por neumatico */}
-                    <select className="w-full bg-gray-50 dark:bg-[#414141] rounded-lg border border-amber-300 p-2 mt-2">
+                    <select className="w-full bg-gray-50 dark:bg-[#414141] rounded-lg border border-amber-300 p-2 mt-2 lg:hidden">
                         <option value="">Seleccione el neumatico</option>
                         {medidasNeumaticosIniciales.map((neumatico, index) => (
                             <option key={index} value={index}>
@@ -71,7 +72,7 @@ export default function Page() {
                         ))}
                     </select>
 
-                    <div className='border bg-gray-50 border-amber-300 rounded-lg p-2 my-2 py-4'>
+                    <div className='border bg-gray-50 border-amber-300 rounded-lg p-2 my-2 py-4 lg:hidden'>
                         <p className='font-semibold'>Codigo del neumatico: WHE393</p>
                         <p>Remanente de la goma</p>
                         {/* Medicion exterior */}
@@ -179,68 +180,67 @@ export default function Page() {
                             </div>
                         </div>
                     </div>
-                    {/* Posiciones faltantes */}
-                    <p className="text-sm text-gray-700 font-semibold dark:text-white">Posiciones restantes</p>
-                    <p className="text-sm text-gray-700 dark:text-white">Posición 2</p>
-                    <p className="text-sm text-gray-700 dark:text-white">Posición 3</p>
 
                     {/* Tabla version escritorio */}
-                    <table className="w-full shadow-md rounded-lg h-[50%] hidden lg:block">
-                        <thead >
-                            <tr className="bg-amber-200 text-black">
-                                <th className="px-2 py-2 text-left">Pos</th>
-                                <th className="px-2 py-2 text-center">Ext</th>
-                                <th className="px-2 py-2 text-center">Int</th>
-                                <th className="px-2 py-2 text-center">Pre.</th>
-                                <th className="px-2 py-2 text-center">Tem.</th>
-                                <th className="px-2 py-2 text-center">Res.</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {nuevasMedidas.map((neumatico, index) => (
-                                <tr key={index} className="border-b border-gray-300">
-                                    <td className="px-2 py-2 text-center">{neumatico.pos}</td>
-                                    {["ext", "int", "pre", "tem"].map((field) => (
-                                        <td key={field} className="px-1 py-2 text-center">
-                                            <div className="inline-flex items-center justify-center gap-1">
-                                                {/* Botón de disminuir */}
-                                                <button
-                                                    onClick={() => handleInputChange(index, field, neumatico[field as keyof typeof neumatico] - 1)}
-                                                    className="bg-amber-100 border-2 border-amber-500 text-black p-2 rounded-lg text-sm"
-                                                >
-                                                    -
-                                                </button>
-
-                                                {/* Input numérico */}
-                                                <input
-                                                    type="number"
-                                                    value={neumatico[field as keyof typeof neumatico]}
-                                                    onChange={(e) => handleInputChange(index, field, parseFloat(e.target.value) || 0)}
-                                                    className="w-16 text-center bg-gray-50 dark:bg-[#414141] rounded-lg border p-2"
-                                                />
-
-                                                {/* Botón de aumentar */}
-                                                <button
-                                                    onClick={() => handleInputChange(index, field, neumatico[field as keyof typeof neumatico] + 1)}
-                                                    className="bg-amber-300 text-black p-2 rounded-lg text-sm"
-                                                >
-                                                    +
-                                                </button>
-                                            </div>
-                                        </td>
-                                    ))}
-                                    <td className="px-2 py-2 text-center">
-                                        <button
-                                            onClick={() => handleResetRow(index)}
-                                            className="bg-red-500 text-white px-3 py-1 rounded-lg text-sm flex items-center gap-1 mx-auto"
-                                        >
-                                            <FaRedo /> Reset
-                                        </button>
-                                    </td>
+                    <div className='hidden lg:block w-[100%]'>
+                        <table className="w-full shadow-md rounded-lg h-[50%] ">
+                            <thead >
+                                <tr className="bg-amber-200 text-black">
+                                    <th className="px-2 py-2 text-left">Pos</th>
+                                    <th className="px-2 py-2 text-center">Ext</th>
+                                    <th className="px-2 py-2 text-center">Int</th>
+                                    <th className="px-2 py-2 text-center">Pre.</th>
+                                    <th className="px-2 py-2 text-center">Tem.</th>
+                                    <th className="px-2 py-2 text-center">Res.</th>
                                 </tr>
-                            ))}
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody className='bg-gray-50 dark:bg-[#414141] w-[100%]'>
+                                {nuevasMedidas.map((neumatico, index) => (
+                                    <tr key={index} className="border-b border-gray-300">
+                                        <td className="px-2 py-2 text-center">{neumatico.pos}</td>
+                                        {["ext", "int", "pre", "tem"].map((field) => (
+                                            <td key={field} className="px-1 py-2 text-center">
+                                                <div className="inline-flex items-center justify-center ">
+                                                    {/* Botón de disminuir */}
+                                                    <button
+                                                        onClick={() => handleInputChange(index, field, neumatico[field as keyof typeof neumatico] - 1)}
+                                                        className="bg-amber-50 border border-amber-500 text-black p-2 h-12 rounded-l-lg text-sm"
+                                                    >
+                                                        -
+                                                    </button>
+                                                    {/* Input numérico */}
+                                                    <input
+                                                        type="text"
+                                                        value={neumatico[field as keyof typeof neumatico]}
+                                                        onChange={(e) => handleInputChange(index, field, parseFloat(e.target.value) || 0)}
+                                                        className="w-20 text-center bg-white dark:bg-[#414141]  border-y border-y-amber-300 p-2 h-12"
+                                                    />
+
+                                                    {/* Botón de aumentar */}
+                                                    <button
+                                                        onClick={() => handleInputChange(index, field, neumatico[field as keyof typeof neumatico] + 1)}
+                                                        className="bg-amber-50 text-black p-2 h-12 rounded-r-lg text-sm border border-amber-500"
+                                                    >
+                                                        +
+                                                    </button>
+                                                </div>
+                                            </td>
+                                        ))}
+
+
+                                        <td className="px-2 py-2 text-center">
+                                            <button
+                                                onClick={() => handleResetRow(index)}
+                                                className="bg-red-200 border border-red-500 h-12 w-32 text-black font-semibold px-3 py-1 rounded-lg text-sm flex items-center  justify-center gap-2 mx-auto"
+                                            >
+                                                <FaRedo /> Deshacer
+                                            </button>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
 
                 <div>
@@ -248,12 +248,11 @@ export default function Page() {
                     <textarea className="w-full bg-gray-50 dark:bg-[#414141] rounded-lg border border-amber-300 p-2" />
                 </div>
 
-
                 <div className='flex gap-x-4 justify-around'>
                     <button onClick={() => setIsOpen(true)} className="bg-amber-300 text-black w-48 px-4 font-bold py-2 rounded-lg mt-4">Confirmar Datos</button>
                     <button className="bg-amber-50 border border-black font-bold text-black w-48 px-4 py-2 rounded-lg mt-4">Cancelar</button>
                 </div>
-                <small className="text-gray-700 dark:text-white text-xs">*Datos erróneos no serán aceptados por el sistema, <span className='font-bold'>Verifique sus datos</span></small>
+                <small className="text-gray-700 dark:text-white text-xs">*Datos erróneos no serán aceptados por el sistema, <span className='font-bold'>Recuerde verificar sus datos</span></small>
 
                 <Modal isOpen={isOpen} onClose={() => setIsOpen(false)} onConfirm={handleConfirm} title="¿Estás seguro?">
                     <p>¿Quieres confirmar esta acción?</p>
