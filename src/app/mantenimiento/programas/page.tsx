05c040f0d6fd20fd5da2e01cd661e4a65c6955b1
JuanNilo <juan.nilo@alumnos.ucn.cl>
2025-04-08 12:42:59 -0400

    Add date range calculation and enhance UI with new icons; improve layout for better usability

--- src/app/mantenimiento/programas/page.tsx
+++ src/app/mantenimiento/programas/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useState } from "react";
 import { programa_mantenimiento } from "@/mocks/programa.json";
-import { FaAngleLeft, FaAngleRight, FaFileAlt, FaFileExcel, FaPen } from "react-icons/fa";
+import { FaAngleLeft, FaAngleRight, FaFileAlt, FaFileDownload, FaPen, FaPlusCircle } from "react-icons/fa";
 import Link from "next/link";
 import * as XLSX from "xlsx";
 import { FaCircleXmark } from "react-icons/fa6";
@@ -44,6 +44,22 @@ export default function Programas() {
         XLSX.writeFile(workbook, "programa_mantenimiento.xlsx");
     };
 
+    function getRangoDeFechas(semana: number): string {
+        const inicioDelAnio = new Date(new Date().getFullYear(), 0, 1); // 1 de enero
+        const diaInicioSemana = new Date(inicioDelAnio.getTime() + (semana - 1) * 7 * 24 * 60 * 60 * 1000);
+
+        const diaFinSemana = new Date(diaInicioSemana);
+        diaFinSemana.setDate(diaInicioSemana.getDate() + 6);
+
+        const formatear = (fecha: Date) =>
+            fecha.toLocaleDateString("es-CL", {
+                day: "2-digit",
+                month: "short",
+            });
+
+        return `${formatear(diaInicioSemana)} - ${formatear(diaFinSemana)}`;
+    }
+
     const handleConfirm = () => {
         setIsOpen(false); // Cerrar modal después de la acción
     };
@@ -51,37 +67,47 @@ export default function Programas() {
 
     const [semana, setSemana] = useState(12);
     return (
-        <div className="bg-[#f1f1f1] dark:bg-[#313131] p-4 h-screen">
-            <div className=" p-4 bg-white dark:bg-[#212121] rounded-md shadow-lg h-[95%]" >
+        <div className="bg-[#f1f1f1] dark:bg-[#313131] lg:p-4 h-screen">
+            <div className=" lg:p-4 bg-white dark:bg-[#212121] rounded-md shadow-lg h-[95%]" >
 
-                <div className="flex justify-between items-center">
+                <div className="flex flex-col gap-y-2 p-2">
 
-                    <Link href="/mantenimiento/programar-mantenimiento" className="bg-amber-100 w-[25%] text-black border-2 border-amber-500 text-lg font-bold h-12 px-4 rounded-md hover:bg-amber-200 transition-all flex justify-center items-center"
-                    >
-                        Agregar Mantenimiento
-                    </Link>
-                    <section>
-                        <h1 className="text-3xl font-mono text-center font-semibold">Programa neumaticos</h1>
+                    <section className="lg:flex items-center justify-between gap-x-2">
+                        <h1 className="text-2xl font-mono font-bold">Programa Semanal de Mantenciones</h1>
 
-                        {/* Agrega un selector de semana, donde salga al medio semana {actual} y al lado 2 botones para cambiar entre semanas */}
-                        <div className="flex justify-center items-center gap-4">
-                            <button className="bg-amber-300 text-black font-bold py-1 px-2 rounded-md" onClick={() => setSemana(semana - 1)}>
+                        <div className="flex justify-center items-center w-[100%] lg:w-[50%]">
+                            <button className="bg-amber-300 text-black font-bold py-1 px-2 rounded-l-md h-10" onClick={() => setSemana(semana - 1)}>
                                 <FaAngleLeft className="text-2xl" />
                             </button>
-                            <h2 className="text-2xl font-mono text-center font-semibold">Semana {semana}</h2>
-                            <button className="bg-amber-300 text-black font-bold py-1 px-2 rounded-md" onClick={() => setSemana(semana + 1)}>
+                            <div className="text-center bg-gray-100 p-2 px-4 w-[80%] md:my-2 md:w-[60%] lg:w-[60%] flex justify-around items-center h-10">
+                                <p className="text-md font-mono ">Semana {semana}</p>
+                                <p className="text-lg text-black font-semibold dark:text-gray-300">{getRangoDeFechas(semana)}</p>
+                            </div>
+                            <button className="bg-amber-300 text-black font-bold py-1 px-2 rounded-r-md h-10" onClick={() => setSemana(semana + 1)}>
                                 <FaAngleRight className="text-2xl" />
                             </button>
                         </div>
                     </section>
-                    <div className="flex justify-end my-4 w-[25%]">
-                        <button
-                            className="bg-amber-100 border-2 border-amber-600 flex w-[100%] justify-center items-center h-12 gap-x-2 text-black font-bold py-4 px-4 rounded-md hover:bg-amber-200 transition-all"
-                            onClick={exportToExcel}
-                        >
-                            Descargar <FaFileExcel className="text-2xl" />
-                        </button>
+                    <div>
+                        {/* botones de agregar mantenimientio y descargar */}
+                        <section className="lg:flex gap-y-2 items-center justify-between gap-x-2 md:w-[70%] mx-auto lg:mx-0">
+
+                            <Link href="/mantenimiento/programar-mantenimiento" className="bg-amber-300 lg:w-[50%] text-black text-lg font-bold h-10 px-4 rounded-md hover:bg-amber-200 transition-all flex justify-center gap-x-2 items-center"
+                            >
+                                <FaPlusCircle className="text-2xl" />
+                                Agregar Mantenimiento
+                            </Link>
+                            <button
+                                className="bg-amber-50 border border-amber-300 flex w-[100%] my-2 lg:w-[50%] justify-center items-center h-10 gap-x-2 text-black font-bold py-4 px-4 rounded-md hover:bg-amber-100 transition-all"
+                                onClick={exportToExcel}
+                            >
+                                <FaFileDownload className="text-2xl" />
+                                <p>Descargar</p>
+                            </button>
+                        </section>
+
                     </div>
+
 
                 </div>
                 {/* Programa semanal */}
@@ -100,9 +126,6 @@ export default function Programas() {
                                 </th>
                                 <th scope="col" className="px-2 py-3">
                                     Fecha
-                                </th>
-                                <th scope="col" className="px-6 py-3">
-                                    Duracion
                                 </th>
                                 <th scope="col" className="px-6 py-3">
                                     Acciones
@@ -144,9 +167,6 @@ export default function Programas() {
                                         </td>
                                         <td className="px-6 py-4 whitespace-nowrap">
                                             <div className="text-sm ">{programa.fecha}</div>
-                                        </td>
-                                        <td className="px-6 py-4 whitespace-nowrap">
-                                            <div className="text-sm ">{programa.duracion}</div>
                                         </td>
                                         <td className="px-6 py-4 whitespace-nowrap">
                                             <div className="text-sm flex justify-between items-center ">
