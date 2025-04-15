5ad23634edd8abc8983df789572fc93d75a897c8
JuanNilo <juan.nilo@alumnos.ucn.cl>
2025-04-15 11:43:07 -0400

    Add logging to middleware for request path tracking

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,7 @@ import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
     // Middleware temporalmente desactivado
+    console.log("Middleware ejecutado", request.nextUrl.pathname);
     return NextResponse.next();
 
     /*
