d49eff9e6ca4a27a5c4497681dd3fe89e5393ada
JuanNilo <juan.nilo@alumnos.ucn.cl>
2025-05-28 09:43:31 -0400

    FIX: Modelo Sensor

---
+++ src/app/modelos/modelo-sensor/page.tsx
@@ -0,0 +1,8 @@
+export default function ModeloSensorPage() {
+    return (
+        <div className="bg-white dark:bg-[#212121] p-6 rounded-md shadow-lg max-w-2xl w-full">
+            <h1>Modelo Sensor</h1>
+            <p>Esta es la página del modelo de sensor.</p>
+        </div>
+    );
+}
