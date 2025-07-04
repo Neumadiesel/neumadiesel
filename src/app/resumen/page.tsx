c1b8d1c1d78641cdd0901cb8d6c7a2ed33f77c3f
JuanNilo <juan.nilo@alumnos.ucn.cl>
2025-07-04 00:22:24 -0400

    refactor: Comment out unused OldTyres import in page.tsx

--- src/app/resumen/page.tsx
+++ src/app/resumen/page.tsx
@@ -1,10 +1,10 @@
-import OldTyres from "@/components/common/charts/neumatico/OldTyres";
+// import OldTyres from "@/components/common/charts/neumatico/OldTyres";
 import TyresKPI from "@/components/features/res/KPI_Operational_Scrapped";
 import TireHealthDashboard from "@/components/features/res/TireHealthDashboard";
 import TreadWearChart from "@/components/features/res/TreadWearChart";
 export default function Page() {
     return (
-        <div className="flex flex-col overflow-x-hidden bg-white dark:bg-[#212121] pt-4 p-2 dark:text-white  w-full o lg:p-3">
+        <div className="flex flex-col overflow-x-hidden lg:gap-y-5 bg-white dark:bg-[#212121] pt-4 p-2 dark:text-white  w-full o lg:p-3">
             <main className="w-[100%] lg:rounded-md mx-auto lg:p-4 ">
                 <div className="grid grid-cols-1 lg:flex w-full justify-between gap-2">
                     <h1 className="text-2xl font-bold">Resumen de Neumáticos</h1>
@@ -14,18 +14,15 @@ export default function Page() {
                 </p>
             </main>
             <TyresKPI />
-
             <TreadWearChart />
             <TireHealthDashboard />
             {/* <OldTyres /> */}
-
             {/* <TreadWearChart /> */}
             {/* <ScrapTyres />
             <OperationalTyres /> */}
             {/* <OperationalTyresHistograms /> */}
             {/* <ScrappedTyresChart /> */}
             {/* <ScrappedReaasonsChart /> */}
-
             {/* <TireAnalyticsDashboard /> */}
         </div>
     );
